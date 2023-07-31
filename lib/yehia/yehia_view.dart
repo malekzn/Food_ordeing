fa29e6457abf1d5484603e9c12b5207f0006734a
Yehia Al-Sabsabi <yehiaalsbsbsi@gmail.com>
2023-07-31 19:11:42 +0300

    Edit From Yhea0

---
+++ lib/yehia/yehia_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body: Center(
+        child: Text('Login'),
+      )),
+    );
+  }
+}
