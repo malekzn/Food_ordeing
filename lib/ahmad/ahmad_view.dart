fad1ef01cede04f82a37eb558d7e9e983d54d5af
Ahmad Zakria <ahmadzeko2018@gmial.com>
2023-07-31 19:02:12 +0300

    comment in main

---
+++ lib/ahmad/ahmad_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class AhmadView extends StatelessWidget {
+  const AhmadView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text('Ahmad Zakria'),)
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
-
+// Allawi hapip kkalbi abo hussin   add by ahmad
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
